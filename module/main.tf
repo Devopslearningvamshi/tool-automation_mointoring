e519d1006f72401df772b8d1a4c23a51088b8b42
patelvamshidhar <reddyvamshi607@gmail.com>
2024-05-26 12:57:43 +0530

    udpated the main.tf

--- module/main.tf
+++ module/main.tf
@@ -2,9 +2,10 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
-}
-tags = {
-  Name = var.tool_name
+
+  tags = {
+    Name = var.tool_name
+  }
 }
 
 resource "aws_route53_record" "record" {
